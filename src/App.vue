1e5022b80f26485656176fa9bf84800a4f2fe8c9
kuryart <kuryart1@gmail.com>
2022-07-11 21:30:38 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Calculator from './components/Calculator.vue'
+</script>
+
+<template>
+  <header>
+    <h1 class="title">A really simple calculator made with Vue</h1>
+  </header>
+
+  <main>
+    <Calculator />
+  </main>
+</template>
+
+<style>
+@import './assets/base.css';
+
+.title {
+  text-align: center;
+  color: #fff;
+  margin: 0;
+  padding: 10vh 0 0 0;
+}
+</style>
